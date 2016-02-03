cc0bf0fdb71b91501d9a3504b4d6724573917e4e
Luis Rodrigues <goblindegook@users.noreply.github.com>
2016-02-03 03:07:23 +0000

    Increase Mocha timeout

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('test', ['pre-test'], function (cb) {
 
   gulp.src('test/**/*.js')
     .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
+    .pipe(mocha({timeout: 7000, reporter: 'spec'}))
     .on('error', function (err) {
       mochaErr = err;
     })
