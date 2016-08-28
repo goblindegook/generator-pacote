d83584cf321ac0ffdd6aed889db294cc5e01f420
Luis Rodrigues <goblindegook@users.noreply.github.com>
2016-08-28 15:44:10 +0100

    AVA: same -> deepEqual

--- generators/app/templates/test/index.js
+++ generators/app/templates/test/index.js
@@ -2,5 +2,5 @@ import test from 'ava'
 import <%= camelCaseName %> from '../src/'
 
 test('sample test', t => {
-  t.same(<%= camelCaseName %>(), true)
+  t.deepEqual(<%= camelCaseName %>(), true)
 })
